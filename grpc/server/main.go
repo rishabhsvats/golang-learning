ee46ee2d0ad4c29ec1b494211fd95062d27fbb18
Rishabh Singh <rishabhsvats@gmail.com>
2024-08-23 15:44:00 +0530

    main server created with grpc and greetserviceserver registered

--- grpc/server/main.go
+++ grpc/server/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/rishabhsvats/grpc/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+type helloserver struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloserver{})
+	log.Printf("server started at %v", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start: %v", err)
+	}
+}
