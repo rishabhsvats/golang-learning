c80320b2febd48a28afb822f40fc1cb8c12181a8
Rishabh Singh <rishabhsvats@gmail.com>
2024-08-30 16:42:28 +0530

    initial comment - Comment struct, createcomment function done

---
+++ go-kafka/producer/producer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Comment struct {
+	Text string `form:"text" json:"text"`
+}
+
+func main() {
+	app := fiber.New()
+	api := app.Group("/api/v1")
+	api.Post("/comments", createComment)
+	app.Listen(":3000")
+}
+
+func createComment(c *fiber.Ctx) error {
+	cmt := new(Comment)
+	if err := c.BodyParser(cmt); err != nil {
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+		return err
+	}
+	cmtInBytes, err := json.Marshal(cmt)
+	PushCommentToQueue("comments", cmtInBytes)
+	err = c.JSON(&fiber.Map{
+		"success": true,
+		"message": "comment pushed successfully",
+		"comment": cmt,
+	})
+	if err != nil {
+		c.Status(500).JSON(&fiber.Map{
+			"succes":  false,
+			"message": "Error creating product",
+		})
+		return err
+	}
+	return nil
+}
