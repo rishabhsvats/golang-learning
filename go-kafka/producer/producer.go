23a2266d88ca68d743d882c6cd421dc80f1134a5
Rishabh Singh <rishabhsvats@gmail.com>
2024-09-02 14:50:36 +0530

    producer and worker done

--- go-kafka/producer/producer.go
+++ go-kafka/producer/producer.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 
+	"github.com/IBM/sarama"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -16,6 +18,38 @@ func main() {
 	api := app.Group("/api/v1")
 	api.Post("/comments", createComment)
 	app.Listen(":3000")
+}
+
+func ConnectProducer(brokerUrl []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+
+	conn, err := sarama.NewSyncProducer(brokerUrl, config)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func PushCommentToQueue(topic string, message []byte) error {
+	brokerUrl := []string{"localhost:29092"}
+	producer, err := ConnectProducer(brokerUrl)
+	if err != nil {
+		return err
+	}
+	defer producer.Close()
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
+	return nil
 }
 
 func createComment(c *fiber.Ctx) error {
