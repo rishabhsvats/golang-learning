7a1bfbc25e78c2e84ed61d128b9957bd0c1db69d
Rishabh Singh <rishabhsvats@gmail.com>
2024-10-08 12:21:25 +0530

    creating dns server

--- dns-resolver/cmd/dns-resolver/main.go
+++ dns-resolver/cmd/dns-resolver/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+
+	"github.com/rishabhsvats/dns-resolver/pkg/dns"
+)
 
 func main() {
 	fmt.Printf("Starting DNS Server...\n")
+	packetConnection, err := net.ListenPacket("udp", ":53")
+	if err != nil {
+		panic(err)
+	}
+	defer packetConnection.Close()
+	for {
+		buf := make([]byte, 512)
+		bytesRead, addr, err := packetConnection.ReadFrom(buf)
+		if err != nil {
+			fmt.Printf("read error from %s: %s", addr.String(), err)
+			continue
+		}
+		go dns.HandlePacket(packetConnection, addr, buf[:bytesRead])
+	}
 }
--- dns-resolver/pkg/dns/resolver.go
+++ dns-resolver/pkg/dns/resolver.go
@@ -12,6 +12,10 @@ import (
 
 const ROOT_SERVERS = "198.41.0.4,199.9.14.201,192.33.4.12,199.7.91.13,192.203.230.10,192.5.5.241,192.112.36.4,198.97.190.53"
 
+func HandlePacket(pc net.PacketConn, addr net.Addr, buf []byte) {
+
+}
+
 func handlePacket(pc net.PacketConn, addr net.Addr, buf []byte) error {
 	return fmt.Errorf("implementation in progress")
 }
