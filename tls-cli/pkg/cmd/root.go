b919e8069a9d8cad0cfbd973026fcecbd4283152
Rishabh Singh <rishabhsvats@gmail.com>
2024-11-12 16:02:19 +0530

    create key command created

--- tls-cli/pkg/cmd/root.go
+++ tls-cli/pkg/cmd/root.go
@@ -2,18 +2,26 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
+	"github.com/rishabhsvats/tls-cli/pkg/cert"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
+
+type Config struct {
+	CACert *cert.CACert          `yaml:"caCert"`
+	Cert   map[string]*cert.Cert `yaml:"certs"`
+}
+
+var cfgFilePath string
+var config Config
 
 var rootCmd = &cobra.Command{
 	Use:   "tls",
 	Short: "tls is a command line tool for tls",
 	Long:  `tls is a command line tool to create certificate, cacertificate and can also be extended`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-	},
 }
 
 func Execute() {
@@ -21,4 +29,27 @@ func Execute() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVarP(&cfgFilePath, "config", "c", "", "config file($default is tls.yaml)")
+}
+
+func initConfig() {
+
+	if cfgFilePath == "" {
+		cfgFilePath = "tls.yaml"
+	}
+	cfgFileBytes, err := ioutil.ReadFile(cfgFilePath)
+	if err != nil {
+		fmt.Printf("error while reading config file: %s", err)
+		return
+	}
+	err = yaml.Unmarshal(cfgFileBytes, &config)
+	if err != nil {
+		fmt.Printf("error while parsing the config file: %s", err)
+		return
+	}
+	fmt.Printf("config file parsed: %v\n", config)
 }
