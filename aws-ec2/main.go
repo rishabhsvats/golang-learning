658da4784d329dccbfc7442b333c348469bac984
Rishabh Singh <rishabhsvats@gmail.com>
2024-11-22 17:46:26 +0530

    creating ec2client and filtering images

--- aws-ec2/main.go
+++ aws-ec2/main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
 func main() {
@@ -26,5 +29,32 @@ func createEC2(ctx context.Context, region string) (string, error) {
 	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
 	if err != nil {
 		return "", fmt.Errorf("unable to load SDK config, %s", err)
+	}
+	ec2Client := ec2.NewFromConfig(cfg)
+
+	keyPairOutput, err := ec2Client.CreateKeyPair(ctx, &ec2.CreateKeyPairInput{
+		KeyName: aws.String("go-aws-demo"),
+	})
+	if err != nil {
+		return "", fmt.Errorf("CreateKeyPair error: %s", err)
+	}
+	imageOutput, err := ec2Client.DescribeImages(ctx, &ec2.DescribeImagesInput{
+		Filters: []types.Filter{
+			{
+				Name:   aws.String("name"),
+				Values: []string{"ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"},
+			},
+			{
+				Name:   aws.String("virtualization-type"),
+				Values: []string{"hvm"},
+			},
+		},
+		Owners: []string{"099720109477"},
+	})
+	if err != nil {
+		return "", fmt.Errorf("DescribeImages error: %s", err)
+	}
+	if len(imageOutput.Images) == 0 {
+		return "", fmt.Errorf("imageOutput.Images is 0 length")
 	}
 }
