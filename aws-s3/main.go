7ffcb11bcced686858ae2bd905dfe2b879baf5f2
Rishabh Singh <rishabhsvats@gmail.com>
2024-12-02 16:25:17 +0530

    function to download file from s3

--- aws-s3/main.go
+++ aws-s3/main.go
@@ -21,6 +21,7 @@ func main() {
 	var (
 		s3Client *s3.Client
 		err      error
+		out      []byte
 	)
 	ctx := context.Background()
 	if s3Client, err = initS3Client(ctx); err != nil {
@@ -36,6 +37,11 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("Upload complete.\n")
+	if out, err = downloadFromS3Bucket(ctx, s3Client); err != nil {
+		fmt.Printf("uploadToS3Bucket error: %s", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Download complete: %s\n", out)
 
 }
 
@@ -88,4 +94,22 @@ func uploadToS3Bucket(ctx context.Context, s3Client *s3.Client) error {
 		return fmt.Errorf("upload error: %s", err)
 	}
 	return nil
+}
+
+func downloadFromS3Bucket(ctx context.Context, s3Client *s3.Client) ([]byte, error) {
+	downloader := manager.NewDownloader(s3Client)
+	buffer := manager.NewWriteAtBuffer([]byte{})
+
+	numBytes, err := downloader.Download(ctx, buffer, &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String("test.txt"),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("download error: %s", err)
+	}
+
+	if numBytesReceived := len(buffer.Bytes()); numBytes != int64(numBytesReceived) {
+		return nil, fmt.Errorf("numbytes received doesn't match: %d vs %d", numBytes, numBytesReceived)
+	}
+	return buffer.Bytes(), nil
 }
