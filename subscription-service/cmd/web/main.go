6d4d9a7380bdbfe956cb6184fd2aee636d3bea6b
Rishabh Singh <rishabhsvats@gmail.com>
2025-03-07 16:20:47 +0530

    starting subscription service project

---
+++ subscription-service/cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort = "80"
+
+func main() {
+
+	//connect to the database
+
+	// create sessions
+
+	//create channels
+
+	//create waitgroup
+
+	//set up the application config
+
+	//setup mail
+
+	//listen for web connections
+}
