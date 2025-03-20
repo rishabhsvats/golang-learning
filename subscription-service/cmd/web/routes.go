b07c542498afeb08c84aa921bf17867c1a6744ce
Rishabh Singh <rishabhsvats@gmail.com>
2025-03-20 17:28:33 +0530

    added login and register endpoints

--- subscription-service/cmd/web/routes.go
+++ subscription-service/cmd/web/routes.go
@@ -11,8 +11,13 @@ func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
-
+	mux.Use(app.SessionLoad)
 	mux.Get("/", app.HomePage)
-
+	mux.Get("/login", app.LoginPage)
+	mux.Post("/login", app.PostLoginPage)
+	mux.Get("/logout", app.Logout)
+	mux.Get("/register", app.RegisterPage)
+	mux.Post("/register", app.PostRegisterPage)
+	mux.Get("/activate", app.ActivateAccount)
 	return mux
 }
