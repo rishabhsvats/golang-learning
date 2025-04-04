2744d63f54e0f2e11e76aca037c7efa8ae77dbcd
Rishabh Singh <rishabhsvats@gmail.com>
2025-04-04 17:26:22 +0530

    subscription page

--- subscription-service/cmd/web/handlers.go
+++ subscription-service/cmd/web/handlers.go
@@ -151,3 +151,24 @@ func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 	//send an email with invoice attached
 	//subscribe the user to an account
 }
+
+func (app *Config) ChooseSubscription(w http.ResponseWriter, r *http.Request) {
+	if !app.Session.Exists(r.Context(), "userID") {
+		app.Session.Put(r.Context(), "warning", "You must log in to see this page!")
+		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		return
+	}
+
+	plans, err := app.Models.Plan.GetAll()
+	if err != nil {
+		app.ErrorLog.Println(err)
+		return
+	}
+
+	dataMap := make(map[string]any)
+	dataMap["plans"] = plans
+
+	app.render(w, r, "plans.page.gohtml", &TemplateData{
+		Data: dataMap,
+	})
+}
