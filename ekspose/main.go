f6c8d6b1a96b94fc4d2648b1a7625405c214a3c1
Rishabh Singh <rishabhsvats@gmail.com>
2025-05-05 16:37:29 +0530

    custom controller ekspose

---
+++ ekspose/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+
+	kubeconfig := flag.String("kubeconfig", "/home/rishabh/.kube/config", "location to your kubeconfig file")
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		// handle error
+		fmt.Printf("erorr %s building config from flags\n", err.Error())
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			fmt.Printf("error %s, getting inclusterconfig", err.Error())
+		}
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	fmt.Println(clientset)
+	if err != nil {
+		// handle error
+		fmt.Printf("error %s, creating clientset\n", err.Error())
+	}
+}
