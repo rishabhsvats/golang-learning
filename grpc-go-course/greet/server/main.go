4c78bdfa6e21e603d0c9f86cb3d2412999983dda
Rishabh Singh <rishabhsvats@gmail.com>
2025-05-07 18:19:54 +0530

    adding basic code to start grpc server

---
+++ grpc-go-course/greet/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/rishabhsvats/golang-learning/grpc-go-course/greet/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", err)
+
+	s := grpc.NewServer()
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve %v\n", err)
+	}
+}
