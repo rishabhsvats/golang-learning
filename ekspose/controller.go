4019e5559017ee0ef9bc63d38b68ca76afb84b79
Rishabh Singh <rishabhsvats@gmail.com>
2025-05-08 17:55:23 +0530

    basic skeleton of controller to handle add and del events

--- ekspose/controller.go
+++ ekspose/controller.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/wait"
+	appsinformers "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
 	appslisters "k8s.io/client-go/listers/apps/v1"
 	"k8s.io/client-go/tools/cache"
@@ -14,7 +19,42 @@ type controller struct {
 	queue         workqueue.RateLimitingInterface
 }
 
-func newController() *controller {
-	c := &controller{}
+func newController(clientset kubernetes.Interface, depInformer appsinformers.DeploymentInformer) *controller {
+	c := &controller{
+		clientset:     clientset,
+		depLister:     depInformer.Lister(),
+		depCacheSyncd: depInformer.Informer().HasSynced,
+		queue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "ekspose"),
+	}
+
+	depInformer.Informer().AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc:    handleAdd,
+			DeleteFunc: handleDel,
+		},
+	)
 	return c
+}
+
+func (c *controller) run(ch <-chan struct{}) {
+	fmt.Println("starting controller")
+	if !cache.WaitForCacheSync(ch, c.depCacheSyncd) {
+		fmt.Printf("error waiting for cache to be synced\n")
+	}
+
+	go wait.Until(c.worker, 1*time.Second, ch)
+
+	<-ch
+}
+
+func (c *controller) worker() {
+
+}
+
+func handleAdd(obj interface{}) {
+	fmt.Println("add was called")
+}
+
+func handleDel(obj interface{}) {
+	fmt.Println("delete was called")
 }
