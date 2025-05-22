fd25a7ed22cc3bc1b33d44fd4100d44600bf6460
Rishabh Singh <rishabhsvats@gmail.com>
2025-05-22 13:54:25 +0530

    creating the basic scaffolding for kluster operator

---
+++ kluster/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rishabhsvats/golang-learning/kluster/pkg/apis/rishabhsvats.dev/v1alpha1"
+)
+
+func main() {
+	k := v1alpha1.Kluster{}
+	fmt.Println(k)
+}
