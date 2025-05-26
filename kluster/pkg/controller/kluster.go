95a9f37961e2018aec5210cc2a54ff611b4c0521
Rishabh Singh <rishabhsvats@gmail.com>
2025-05-26 18:43:29 +0530

    started working on controller feature

---
+++ kluster/pkg/controller/kluster.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"log"
+
+	klientset "github.com/rishabhsvats/golang-learning/kluster/pkg/generated/clientset/versioned"
+	kinf "github.com/rishabhsvats/golang-learning/kluster/pkg/generated/informers/externalversions/rishabhsvats.dev/v1alpha1"
+	klister "github.com/rishabhsvats/golang-learning/kluster/pkg/generated/listers/rishabhsvats.dev/v1alpha1"
+	"k8s.io/client-go/tools/cache"
+
+	"k8s.io/client-go/util/workqueue"
+)
+
+type Controller struct {
+	// clientset for custom resource kluster
+	klient klientset.Interface
+	// cluster cache has synced
+	klusterSynced cache.InformerSynced
+	// lister
+	kLister klister.KlusterLister
+	// queue
+	wq workqueue.RateLimitingInterface
+}
+
+func NewController(klient klientset.Interface, klusterInformer kinf.KlusterInformer) *Controller {
+	c := &Controller{
+		klient:        klient,
+		klusterSynced: klusterInformer.Informer().HasSynced,
+		kLister:       klusterInformer.Lister(),
+		wq:            workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "kluster"),
+	}
+
+	klusterInformer.Informer().AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc:    c.handleAdd,
+			DeleteFunc: c.handleDel,
+		},
+	)
+	return c
+}
+
+func (c *Controller) handleAdd(obj interface{}) {
+	log.Println("handleAdd was called")
+	c.wq.Add(obj)
+}
+
+func (c *Controller) handleDel(obj interface{}) {
+	log.Println("handleDel was called")
+	c.wq.Add(obj)
+}
