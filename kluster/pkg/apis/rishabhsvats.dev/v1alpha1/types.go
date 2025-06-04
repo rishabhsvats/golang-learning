2912eca309814f241c5f766819398419b1eb26c0
Rishabh Singh <rishabhsvats@gmail.com>
2025-06-04 16:54:23 +0530

    additional printer column added to add the clusterid and progress

--- kluster/pkg/apis/rishabhsvats.dev/v1alpha1/types.go
+++ kluster/pkg/apis/rishabhsvats.dev/v1alpha1/types.go
@@ -7,6 +7,8 @@ import (
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="ClusterID",type=string,JSONPath=`.status.klusterID`
+// +kubebuilder:printcolumn:name="Progress",type=string,JSONPath=`.status.progress`
 type Kluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
