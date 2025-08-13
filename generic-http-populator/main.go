86892536b58407a2331f577a8ec3675b4643c09f
Rishabh Singh <rishabhsvats@gmail.com>
2025-08-13 12:27:02 +0530

    controller function to start pod and call populator

--- generic-http-populator/main.go
+++ generic-http-populator/main.go
@@ -1,6 +1,21 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	populatorMachinery "github.com/kubernetes-csi/lib-volume-populator/populator-machinery"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+const (
+	prefix    = "k8s.rishabhsvats.dev"
+	mountPath = "/mnt/vol"
+)
 
 func main() {
 	var image string
@@ -14,4 +29,52 @@ func main() {
 	flag.StringVar(&uri, "uri", "", "URI for the content of the volume")
 
 	flag.Parse()
+
+	switch mode {
+	case "controller":
+		gk := schema.GroupKind{
+			Group: "k8s.rishabhsvats.dev",
+			Kind:  "GenericHTTPPopulator",
+		}
+		gvr := schema.GroupVersionResource{
+			Group:    gk.Group,
+			Version:  "v1alpha1",
+			Resource: "generichttppopulators",
+		}
+		populatorMachinery.RunController("", "", image, "", "", namespace, prefix, gk, gvr, mountPath, "", populatorArgs)
+	case "populator":
+		// the code that we write here is going to get called from populator pod
+		// we know that PVC is mounted at `mountPath`
+		populate(uri)
+	default:
+		log.Printf("Mode %s is not supported", mode)
+	}
+}
+func populate(uri string) {
+
+}
+
+type GenericHTTPPopulator struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+
+	Spec GenericHTTPPopulatorSpec `json:"spec"`
+}
+type GenericHTTPPopulatorSpec struct {
+	URI string `json:"uri"`
+}
+
+// b here specifies if the volume is in block mode
+// u is the populator instance
+func populatorArgs(b bool, u *unstructured.Unstructured) ([]string, error) {
+	var ghp GenericHTTPPopulator
+	var args []string
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.UnstructuredContent(), &ghp)
+	if err != nil {
+		log.Printf("Failed converting unstructured to GHP, error %s\n", err.Error())
+		return args, err
+	}
+	args = append(args, "--mode=populator")
+	args = append(args, fmt.Sprintf("--uri=%s", ghp.Spec.URI))
+	return args, nil
 }
